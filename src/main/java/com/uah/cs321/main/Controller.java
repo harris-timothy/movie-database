fdfb529a97159f090950fa7f2e5ccc627459a57f
harris-timothy <harris.tim608@gmail.com>
2020-04-25 12:42:05 -0500

    Create Controller.java

---
+++ src/main/java/com/uah/cs321/main/Controller.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.uah.cs321.main;
+
+import main.java.com.uah.cs321.movie_database.database.MovieDatabase;
+
+/**
+ *
+ * @author Tim Harris
+ */
+public class Controller extends MovieDatabase {
+    
+}
