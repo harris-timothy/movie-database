6f6ca12a23f2acf78aab9bff699fc35ecf4611cf
TheCactusLord <30806530+TheCactusLord@users.noreply.github.com>
2020-04-25 19:07:24 -0500

    Update realGUIMaybe.java

--- src/main/java/com/uah/cs321/main/realGUIMaybe.java
+++ src/main/java/com/uah/cs321/main/realGUIMaybe.java
@@ -22,6 +22,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import static javax.swing.Spring.width;
 import main.java.com.uah.cs321.movie_database.database.MovieDatabase;
 import main.java.com.uah.cs321.movie_database.database.Movie;
 /**
@@ -29,9 +30,8 @@ import main.java.com.uah.cs321.movie_database.database.Movie;
  */
 public class realGUIMaybe {
     public static void main(String[] args){
-//---------------------------------------------------------------------------------
-        //Declarations
-        String path = System.getProperty("user.dir");
+
+       String path = System.getProperty("user.dir");
         String pathSep = File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator;
         String fileName = "MovieDataTrimmed.csv";
         String finalFile = path  + pathSep + fileName;
@@ -47,26 +47,48 @@ public class realGUIMaybe {
         JList<Movie> movieList = new JList<Movie>(tempArr);
         movieList.setBounds(20, 15, 300, 300);
         
-
-        
-        JFrame mainFrame = new JFrame("Main Frame");
+ JFrame mainFrame = new JFrame("Main Frame");
         JFrame descriptionFrame = new JFrame("Movie Description");
         JFrame searchResultsFrame = new JFrame("Search Results");
-        JPanel mainPanel = new JPanel();
         JPanel descriptionPanel = new JPanel();
+        JPanel mainPanel = new JPanel();
         JPanel searchPanel = new JPanel();
         JLabel mainLabel = new JLabel();
         JLabel textLabel = new JLabel();
+        JLabel searchLabel = new JLabel();
         JTextField mainTextField= new JTextField(50);
         JButton searchButton = new JButton("GO");
+        JButton quitButton = new JButton("QUIT");
         mainPanel.setLayout(null);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int height = screenSize.height;
-        int width = screenSize.width;
-        mainLabel.setText("SEARCH");
+
         mainFrame.setResizable(false);
         descriptionFrame.setResizable(false);
         searchResultsFrame.setResizable(false);
+        searchButton.setBackground(Color.white);
+        mainPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        movieList.setBorder(BorderFactory.createLineBorder(Color.black));
+//---------------------------------------------------------------------------------
+        mainFrame.setContentPane(mainPanel);
+        searchResultsFrame.setContentPane(searchPanel);
+        descriptionFrame.setContentPane(descriptionPanel);
+        searchPanel.add(searchLabel);
+        //mainPanel.add(scrollPane);
+        searchPanel.add(quitButton);
+        mainPanel.add(movieList);
+        mainPanel.add(searchButton);
+        mainPanel.add(mainLabel);
+        mainPanel.add(textLabel);
+	mainPanel.add(mainTextField);
+        mainPanel.add(searchButton);
+        mainFrame.getContentPane().setBackground(Color.lightGray);
+        descriptionFrame.getContentPane().setBackground(Color.lightGray);
+        searchResultsFrame.getContentPane().setBackground(Color.lightGray);
+        mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(700, 700);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
 //---------------------------------------------------------------------------------
         //Main window components
 	mainLabel.setBounds(400, 0, 100, 60);
@@ -74,30 +96,36 @@ public class realGUIMaybe {
         searchButton.setBounds(550, 20, 60, 20);
 	mainTextField.setBounds(450, 20, 100, 20); 
         
-        mainTextField.addKeyListener(new KeyAdapter() {
+        movieList.setBounds(20, 15, 300, 300);
+        quitButton.setBounds(550, 20, 60, 20);
+ //--------------------------------------------------------------------------------- 
+        /*mainTextField.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if(e.getKeyCode() == KeyEvent.VK_ENTER){
-                    String text = mainTextField.getText();
+
                     System.out.println(text);
                 }
             }
         }); 
+        */
+//--------------------------------------------------------------------------------- 
         searchButton.addActionListener(e->{
            searchResultsFrame.setDefaultCloseOperation(searchResultsFrame.DISPOSE_ON_CLOSE);
-           searchResultsFrame.setSize(width/2, height/2);
+           searchResultsFrame.setSize(700, 700);
            searchResultsFrame.setLocationRelativeTo(null);
            searchResultsFrame.setVisible(true);
+           searchLabel.setText("RESULTS...");
+           searchLabel.setBounds(10, 0, 100, 60);
         });
+//--------------------------------------------------------------------------------- 
+        quitButton.addActionListener(e->{
+                searchResultsFrame.dispose();
+        });
+//--------------------------------------------------------------------------------- 
           /* searchButton.addActionListener(new ActionListener(){
             //this is anonymous class
             public void actionPerformed(ActionEvent evt){
-                String text = searchButton.getText();
-                mainTextField.setText(text);
-                System.out.println(text);
-            }
-          @Override
-          public void actionPerformed(java.awt.event.ActionEvent e) {
-              throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+@ -98,20 +128,6 @@ public class realGUIMaybe {
           }
         });
         */
@@ -111,7 +139,7 @@ public class realGUIMaybe {
         mainPanel.add(searchButton);
         mainFrame.getContentPane().setBackground(Color.lightGray);
         mainFrame.setDefaultCloseOperation(mainFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(width/2, height/2);
+        mainFrame.setSize(700, 700);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setVisible(true);
     
